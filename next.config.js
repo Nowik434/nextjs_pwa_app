21a0a62bcab6d2b6c541b4898bc9f3f6f3debb70
Nowik434 <nowik434@gmail.com>
2021-11-22 19:27:28 +0100

    first working verson v0.13

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ module.exports = withPWA({
     AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
     AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME
   },
+  distDir: 'build',
   target: 'serverless',
   eslint: {
     ignoreDuringBuilds: true,
