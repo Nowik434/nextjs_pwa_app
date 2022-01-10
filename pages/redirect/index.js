5a513094b732bc97e1cba2fc9b19e4ff30f6d3fd
Nowik434 <nowik434@gmail.com>
2022-01-10 20:59:28 +0100

    added redirections

--- pages/redirect/index.js
+++ pages/redirect/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Box from '@mui/material/Box';
 
 const Redirect = () => {
     const router = useRouter();
     return (
-        <>
-            <Typography variant="h3" component="h3" gutterBottom align="center">
+        <Box sx={{ mx: "auto", my: 6, textAlign: 'center' }}>
+            <Image src="/static/vcclogo.png" alt="gift" width={200} height={200} />
+            <Typography variant="h5" component="h3" gutterBottom align="center">
                 Dziękujemy za udział w konkursie
             </Typography>
-            <Button variant="contained" onClick={() => router.replace('https://kwalifikacje.vccsystem.eu/?page_id=198')} sx={{ mb: 4 }}>
+            <Button variant="contained" onClick={() => router.replace('https://kwalifikacje.vccsystem.eu/?page_id=198')} sx={{ mt: 6, mb: 4 }}>
                 Przejdź do strony VCC
             </Button>
-        </>
+        </Box>
     );
 }
 
